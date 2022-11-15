1ccfa113f88fa1efd3621f18f1478c375dabcf00
ja1antonio <ja1.negocios77@gmail.com>
2022-11-15 18:59:37 -0400

    Fluindo 2

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -64,7 +64,13 @@ class NotesController {
     if (tags) {
       const filterTags = tags.split(',').map((tag) => tag.trim());
 
-      notes = await knex('tags').whereIn('name', filterTags);
+      notes = await knex('tags')
+        .select(['notes.id', 'notes.title', 'notes.user_id'])
+        .where('notes.user_id', user_id)
+        .whereLike('notes.title', `%${title}%`)
+        .whereIn('name', filterTags)
+        .innerJoin('notes', 'notes.id', 'tags.note_id')
+        .orderBy('notes.title');
     } else {
       notes = await knex('notes')
         .where({ user_id })
@@ -72,7 +78,17 @@ class NotesController {
         .orderBy('title');
     }
 
-    return response.json(notes);
+    const userTags = await knex('tags').where({ user_id });
+    const noteWithTags = notes.map((note) => {
+      const noteTags = userTags.filter((tag) => tag.note_id === note.id);
+
+      return {
+        ...note,
+        tags: noteTags,
+      };
+    });
+
+    return response.json(noteWithTags);
   }
 }
 
