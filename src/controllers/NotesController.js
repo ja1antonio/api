7deddf1c56627927dc3a04fad95027b374792043
ja1antonio <ja1.negocios77@gmail.com>
2022-11-15 14:16:43 -0400

    Fluindo

---
+++ src/controllers/NotesController.js
@@ -0,0 +1,79 @@
+const knex = require('../database/knex');
+
+class NotesController {
+  async create(request, response) {
+    const { title, description, tags, links } = request.body;
+    const { user_id } = request.params;
+
+    const note_id = await knex('notes').insert({
+      title,
+      description,
+      user_id,
+    });
+
+    const linksInset = links.map((link) => {
+      return {
+        note_id,
+        url: link,
+      };
+    });
+
+    await knex('links').insert(linksInset);
+
+    const tagsInset = tags.map((name) => {
+      return {
+        note_id,
+        name,
+        user_id,
+      };
+    });
+    await knex('tags').insert(tagsInset);
+
+    response.json();
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex('notes').where({ id }).first();
+    const tags = await knex('tags').where({ note_id: id }).orderBy('name');
+    const links = await knex('links')
+      .where({ note_id: id })
+      .orderBy('created_at');
+
+    return response.json({
+      ...note,
+      tags,
+      links,
+    });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    await knex('notes').where({ id }).delete();
+
+    return response.json();
+  }
+
+  async index(request, response) {
+    const { title, user_id, tags } = request.query;
+
+    let notes;
+
+    if (tags) {
+      const filterTags = tags.split(',').map((tag) => tag.trim());
+
+      notes = await knex('tags').whereIn('name', filterTags);
+    } else {
+      notes = await knex('notes')
+        .where({ user_id })
+        .whereLike('title', `%${title}%`)
+        .orderBy('title');
+    }
+
+    return response.json(notes);
+  }
+}
+
+module.exports = NotesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
 const userRouter = require('./users.routes');
+const notesRouter = require('./notes.routes');
 
 const routes = Router();
-
 routes.use('/users', userRouter);
+routes.use('/notes', notesRouter);
 
 module.exports = routes;
