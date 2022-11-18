7b4448650260b8153dc2770446032fe1421247dc
ja1antonio <ja1.negocios77@gmail.com>
2022-11-18 09:10:26 -0400

    Parte de integração com fotos finalizada

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -2,16 +2,16 @@ const path = require('path');
 const multer = require('multer');
 const crypto = require('crypto');
 
-const TMP_FOLDER = Path.resolver(__dirname, '', '..', 'tmp');
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp');
 
-const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads');
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads');
 
 const MULTER = {
   storage: multer.diskStorage({
     destination: TMP_FOLDER,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHah}-${file.originalname}`;
+      const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName);
     },
--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ require('express-async-errors');
 const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const express = require('express');
+const uploadConfig = require('./configs/upload');
 const routes = require('./routes');
 migrationsRun();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 
